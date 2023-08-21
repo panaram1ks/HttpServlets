b9fb170fac79f2cb978aaa9d87ce97f69ba16e01
parom <E.Parominskiy@isida.by>
2023-08-21 17:18:10 +0300

    new method mapping servlet @WebServlet("/mapping")

---
+++ servlet-starters/src/main/java/com/dmdev/application/servlet/SecondServlet.java
@@ -0,0 +1,42 @@
+package com.dmdev.application.servlet;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/second")
+public class SecondServlet extends HttpServlet {
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        // Global common object for any servlet !
+        ServletContext servletContext = config.getServletContext();
+        super.init(config);
+    }
+
+//    @Override
+//    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.service(req, resp);
+//    }
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        try (PrintWriter writer = resp.getWriter()) {
+            writer.write("Hello from second servlet");
+        }
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+    }
+}
