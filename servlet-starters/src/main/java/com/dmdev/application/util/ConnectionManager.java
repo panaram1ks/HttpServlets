c5394c14975d418b89cb59c682a89fe7c314832d
parom <E.Parominskiy@isida.by>
2023-08-22 12:15:06 +0300

    lombok utilityClass

--- servlet-starters/src/main/java/com/dmdev/application/util/ConnectionManager.java
+++ servlet-starters/src/main/java/com/dmdev/application/util/ConnectionManager.java
@@ -1,9 +1,13 @@
 package com.dmdev.application.util;
 
+import lombok.SneakyThrows;
+import lombok.experimental.UtilityClass;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+@UtilityClass
 public final class ConnectionManager {
 
     private static final String URL_KEY = "db.url";
@@ -22,17 +26,12 @@ public final class ConnectionManager {
         }
     }
 
-    private ConnectionManager(){}
-
+    @SneakyThrows
     public static Connection get(){
-        try {
             return DriverManager.getConnection(
                     PropertiesUtil.get(URL_KEY),
                     PropertiesUtil.get(USER_KEY),
                     PropertiesUtil.get(PASSWORD_KEY)
             );
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
