5f101e2af86e3a01383e0dce586e7451ef047a52
parom <E.Parominskiy@isida.by>
2023-08-22 10:41:10 +0300

    service level

---
+++ servlet-starters/src/main/java/com/dmdev/application/service/FlightService.java
@@ -0,0 +1,31 @@
+package com.dmdev.application.service;
+
+import com.dmdev.application.dao.FlightDao;
+import com.dmdev.application.dto.FlightDto;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FlightService {
+
+    private static final FlightService INSTANCE = new FlightService();
+
+    private FlightService() {
+    }
+
+    private final FlightDao flightDao = FlightDao.getInstance();
+
+    public List<FlightDto> findAll() {
+        return flightDao.findAll().stream().map(flight -> new FlightDto(
+                flight.getId(),
+                """
+                         %s - %s - %s
+                        """.formatted(flight.getDepartureAirportCode(), flight.getArrivalAirportCode(), flight.getStatus())
+        )).collect(Collectors.toList());
+    }
+
+
+    public FlightService getInstance() {
+        return INSTANCE;
+    }
+}
