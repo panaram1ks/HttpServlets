0910cef2defba2defeca4b42ba3b2781f6f5c68b
parom <E.Parominskiy@isida.by>
2023-08-22 10:38:21 +0300

    dto Flight

---
+++ servlet-starters/src/main/java/com/dmdev/application/dto/FlightDto.java
@@ -0,0 +1,36 @@
+package com.dmdev.application.dto;
+
+import java.util.Objects;
+
+public class FlightDto {
+
+    private final Long id;
+    private final String description;
+
+
+    public FlightDto(Long id, String description) {
+        this.id = id;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FlightDto flightDto = (FlightDto) o;
+        return Objects.equals(id, flightDto.id) && Objects.equals(description, flightDto.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, description);
+    }
+}
