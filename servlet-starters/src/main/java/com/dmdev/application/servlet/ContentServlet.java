5bb2956b7e7812860ec557a70dfb74cfcb2a92df
parom <E.Parominskiy@isida.by>
2023-08-22 16:29:49 +0300

    expression language EL

--- servlet-starters/src/main/java/com/dmdev/application/servlet/ContentServlet.java
+++ servlet-starters/src/main/java/com/dmdev/application/servlet/ContentServlet.java
@@ -1,5 +1,7 @@
 package com.dmdev.application.servlet;
 
+import com.dmdev.application.dto.FlightDto;
+import com.dmdev.application.service.FlightService;
 import com.dmdev.application.util.JspHelper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -8,12 +10,20 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @WebServlet("/content")
 public class ContentServlet extends HttpServlet {
 
+    private final FlightService flightService = FlightService.getInstance();
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<FlightDto> flightDto = flightService.findAll();
+        req.setAttribute("flights", flightDto);
+        req.getSession().setAttribute("flightsMap", flightDto.stream().collect(Collectors.toMap(FlightDto::getId, FlightDto::getDescription)));
+
         // path starts from "webapp folder"
         req.getRequestDispatcher(JspHelper.getPath("content")).forward(req, resp);
     }
--- servlet-starters/src/main/webapp/WEB-INF/jsp/content.jsp
+++ servlet-starters/src/main/webapp/WEB-INF/jsp/content.jsp
@@ -6,7 +6,17 @@
 <body>
 <%@ include file="header.jsp" %>
 <div>
-    content. Русский
+    <span>content. Русский</span>
+    <p>Size: ${requestScope.flights.size()}</p>
+    <p>Id 1th way : ${requestScope.flights.get(0).id}</p>
+    <%--    necessary collection allow access by index!!!   --%>
+    <p>Id 2th way: ${requestScope.flights[1].id}</p>
+    <p>Map: ${sessionScope.flightsMap[1]}</p>
+    <p>JSESSION id: ${cookie["JSESSIONID"].value}, unique identifier</p>
+    <p>Header: ${header["Cookie"]}</p>
+    <p>Param id: ${param.id}</p>
+    <p>Param test: ${param.test}</p>
+    <p>Empty list: ${not empty flights}</p>
 </div>
 <%@ include file="footer.jsp" %>
 </body>
