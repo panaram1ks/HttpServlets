b0f82a3ffce2889a0cad40ff1ae6b2cfd0ddb870
parom <E.Parominskiy@isida.by>
2023-08-22 11:33:10 +0300

    service Ticket

--- servlet-starters/src/main/java/com/dmdev/application/service/TicketService.java
+++ servlet-starters/src/main/java/com/dmdev/application/service/TicketService.java
@@ -1,13 +1,21 @@
 package com.dmdev.application.service;
 
+import com.dmdev.application.dao.TicketDao;
 import com.dmdev.application.dto.TicketDto;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class TicketService {
     private static final TicketService INSTANCE = new TicketService();
 
-    public List<TicketDto>
+    private final TicketDao ticketDao = TicketDao.getInstance();
+
+    public List<TicketDto> findAllByFlightId(Long flightId) {
+        return ticketDao.findAllByFlightId(flightId).stream().map(ticket ->
+                new TicketDto(ticket.getId(), ticket.getFlightId(), ticket.getSeatNo())
+        ).collect(Collectors.toList());
+    }
 
     private TicketService() {
     }
