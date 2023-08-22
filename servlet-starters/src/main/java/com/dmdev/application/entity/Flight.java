c88a3f6af1d8388a48272e9c72d1e886a9723c56
parom <E.Parominskiy@isida.by>
2023-08-22 10:37:57 +0300

    full entity

--- servlet-starters/src/main/java/com/dmdev/application/entity/Flight.java
+++ servlet-starters/src/main/java/com/dmdev/application/entity/Flight.java
@@ -1,6 +1,7 @@
 package com.dmdev.application.entity;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 public class Flight {
     private Long id;
@@ -9,7 +10,108 @@ public class Flight {
     private String departureAirportCode;
     private LocalDateTime arrivalDate;
     private String arrivalAirportCode;
-    private Integer aircraftId;
+    private Long aircraftId;
     private FlightStatus status;
 
+    public Flight(Long id, String flightNo, LocalDateTime departureDate, String departureAirportCode, LocalDateTime arrivalDate, String arrivalAirportCode, Long aircraftId, FlightStatus status) {
+        this.id = id;
+        this.flightNo = flightNo;
+        this.departureDate = departureDate;
+        this.departureAirportCode = departureAirportCode;
+        this.arrivalDate = arrivalDate;
+        this.arrivalAirportCode = arrivalAirportCode;
+        this.aircraftId = aircraftId;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFlightNo() {
+        return flightNo;
+    }
+
+    public void setFlightNo(String flightNo) {
+        this.flightNo = flightNo;
+    }
+
+    public LocalDateTime getDepartureDate() {
+        return departureDate;
+    }
+
+    public void setDepartureDate(LocalDateTime departureDate) {
+        this.departureDate = departureDate;
+    }
+
+    public String getDepartureAirportCode() {
+        return departureAirportCode;
+    }
+
+    public void setDepartureAirportCode(String departureAirportCode) {
+        this.departureAirportCode = departureAirportCode;
+    }
+
+    public LocalDateTime getArrivalDate() {
+        return arrivalDate;
+    }
+
+    public void setArrivalDate(LocalDateTime arrivalDate) {
+        this.arrivalDate = arrivalDate;
+    }
+
+    public String getArrivalAirportCode() {
+        return arrivalAirportCode;
+    }
+
+    public void setArrivalAirportCode(String arrivalAirportCode) {
+        this.arrivalAirportCode = arrivalAirportCode;
+    }
+
+    public Long getAircraftId() {
+        return aircraftId;
+    }
+
+    public void setAircraftId(Long aircraftId) {
+        this.aircraftId = aircraftId;
+    }
+
+    public FlightStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(FlightStatus status) {
+        this.status = status;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Flight flight = (Flight) o;
+        return Objects.equals(id, flight.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Flight{" +
+                "id=" + id +
+                ", flightNo='" + flightNo + '\'' +
+                ", departureDate=" + departureDate +
+                ", departureAirportCode='" + departureAirportCode + '\'' +
+                ", arrivalDate=" + arrivalDate +
+                ", arrivalAirportCode='" + arrivalAirportCode + '\'' +
+                ", aircraftId=" + aircraftId +
+                ", status=" + status +
+                '}';
+    }
 }
