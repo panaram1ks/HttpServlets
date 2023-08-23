9f4a241efe4b8515a870068291f94c2bf3bcac37
parom <E.Parominskiy@isida.by>
2023-08-23 11:31:58 +0300

    Filters error page

--- servlet-starters/src/main/java/com/dmdev/application/filter/CharsetFilter.java
+++ servlet-starters/src/main/java/com/dmdev/application/filter/CharsetFilter.java
@@ -1,6 +1,8 @@
 package com.dmdev.application.filter;
+
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.annotation.WebInitParam;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -8,10 +10,20 @@ import java.nio.charset.StandardCharsets;
 
 /**
  * WE can bondles servlet's url, and Servlet's name
- * */
+ */
 //@WebFilter("/*")
-@WebFilter(servletNames = {"RegistrationServlet"})
+@WebFilter(servletNames = {"RegistrationServlet"},
+        initParams = {
+                @WebInitParam(name = "param1", value = "paramValue")
+        },
+        dispatcherTypes = DispatcherType.REQUEST
+)
 public class CharsetFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
