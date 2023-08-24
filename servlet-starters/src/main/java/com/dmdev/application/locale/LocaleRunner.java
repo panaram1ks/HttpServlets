69470d38bba60b8252d8dd5a541f9a1a25bd2a0a
parom <E.Parominskiy@isida.by>
2023-08-24 08:45:54 +0300

    Internationalization (i18n), Localization (l10n)

---
+++ servlet-starters/src/main/java/com/dmdev/application/locale/LocaleRunner.java
@@ -0,0 +1,18 @@
+package com.dmdev.application.locale;
+
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class LocaleRunner {
+
+    public static void main(String[] args) {
+        Locale locale = new Locale("ru", "RU");
+        System.out.println(Locale.US);
+        System.out.println(Locale.getDefault());
+
+//        ResourceBundle translations = ResourceBundle.getBundle("translations"); // DEFAULT
+        ResourceBundle translations = ResourceBundle.getBundle("translations", locale);
+        System.out.println(translations.getString("page.login.password"));
+    }
+}
